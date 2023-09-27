9787b43ca91ec3357e6955346d509c15777fc534
Avnish <20027avi@gmail.com>
2023-09-27 23:20:27 +0530

    first commit

---
+++ src/BookServiceInterface.java
@@ -0,0 +1,13 @@
+
+public abstract class BookServiceInterface {
+
+    abstract void addBook();
+
+    abstract void showAllBooks();
+
+    abstract void showAllAvailableBooks();
+
+    abstract void borrowBook();
+
+    abstract void returnBook();
+}
